009c17168bb9a02f67310bd4b895730835fd11b8
renhai1 <1457481392@qq.com>
2021-09-15 22:44:37 +0800

    Initial commit

---
+++ src/main/java/controller/ControllerDemo.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @className:controllerDemo1
+ * @description: controllerDemo1
+ * @author: renhai
+ * @create: 2021-09-15 22:01
+ */
+@Controller
+public class ControllerDemo {
+}
